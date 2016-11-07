4974cd5e35cee47e1fb1f18bedde189c5a3966f6
Shivam Kumar <shivamhclme5@gmail.com>
2016-11-07 17:30:25 +0530

    Add files via upload

---
+++ shivam.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class shivam{
+	public static void main(String[] args) {
+		
+		
+		Scanner scn = new Scanner(System.in);
+		
+		boolean same = true;
+	
+		String s1  = "shivam"; 
+		
+		
+		
+		String s2 = "shivam";
+		
+		if(s1 == s2){
+			System.out.println("since s1 ans s2 is equal, so it is "+same);
+		}else{
+			System.out.println("since s1 and s2 is not equal, so it is false");
+		}
+	}
+}
